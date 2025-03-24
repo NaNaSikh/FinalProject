0eb46ff6e8f83e4c941eda2bbdd986d0cf58c494
NaNaSikh <nanasixarulidze78@gmail.com>
2025-03-24 22:55:15 +0400

    add LoggingRepository

---
+++ EmployeeBonusManagementSystem.Application/Contracts/Persistence/ILoggingRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeBonusManagementSystem.Application.Contracts.Persistence
+{
+    public interface ILoggingRepository
+    {
+	    void LogInformation(string message, params object[] args);
+	    void LogError(string message, Exception exception, params object[] args);
+	}
+}
---
+++ EmployeeBonusManagementSystem.Persistence/Repositories/LoggingRepository.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EmployeeBonusManagementSystem.Application.Contracts.Persistence;
+using Microsoft.Extensions.Logging;
+
+namespace EmployeeBonusManagementSystem.Persistence.Repositories
+{
+    public class LoggingRepository : ILoggingRepository
+    {
+	    private readonly ILogger<LoggingRepository> _logger;
+	    private readonly IUnitOfWork _unitOfWork;
+
+
+		public LoggingRepository(ILogger<LoggingRepository> logger, IUnitOfWork unitOfWork)
+	    {
+		    _logger = logger;
+		    _unitOfWork = unitOfWork;
+
+		}
+
+		public void LogInformation(string message, params object[] args)
+	    {
+		    _logger.LogInformation(message, args);
+		    //TODO add to database logic here 
+
+		}
+
+		public void LogError(string message, Exception exception, params object[] args)
+	    {
+		    _logger.LogError(exception, message, args);
+		    //TODO add to database logic here 
+
+		}
+
+	}
+}
