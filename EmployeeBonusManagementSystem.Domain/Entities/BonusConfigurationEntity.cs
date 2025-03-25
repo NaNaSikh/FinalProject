783b9464a8415963c498cac370a6ab9eee6b11d7
Landa Mindiashvili <lmindiashvili@credo.ge>
2025-03-25 23:37:42 +0400

    createByUser

--- EmployeeBonusManagementSystem.Domain/Entities/BonusConfigurationEntity.cs
+++ EmployeeBonusManagementSystem.Domain/Entities/BonusConfigurationEntity.cs
@@ -7,7 +7,8 @@ public class BonusConfigurationEntity : BaseEntity
     public decimal MaxBonusAmount { get; set; }
     public double MaxBonusPercentage { get; set; }
     public double MinBonusPercentage { get; set; }
-    public double ActualBonusPercent { get; set; }
+
+    //public double ActualBonusPercent { get; set; }
     public int MaxRecommendationLevel { get; set; }
     public double RecommendationBonusRate { get; set; }
 
---
+++ EmployeeBonusManagementSystem.Persistence/Configurations/BonusConfigurationEntityConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using EmployeeBonusManagementSystem.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EmployeeBonusManagementSystem.Persistence.Configurations;
+
+public class BonusConfigurationEntityConfiguration : IEntityTypeConfiguration<BonusConfigurationEntity>
+{
+    public void Configure(EntityTypeBuilder<BonusConfigurationEntity> builder)
+    {
+        builder.ToTable("BonusConfigurations");
+
+        builder.HasKey(b => b.Id);
+
+        builder.Property(b => b.MaxBonusAmount)
+            .HasColumnType("decimal(18,2)")
+            .IsRequired();
+
+        builder.Property(b => b.MaxBonusPercentage)
+            .IsRequired();
+
+        builder.Property(b => b.MinBonusPercentage)
+            .IsRequired();
+
+        builder.Property(b => b.MaxRecommendationLevel)
+            .IsRequired();
+
+        builder.Property(b => b.RecommendationBonusRate)
+            .IsRequired();
+    }
+}
